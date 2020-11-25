9c6280e0c287be30a783c732dc7549183341e0c5
Thorsten <t.burandt@outlook.de>
2020-11-25 15:13:28 +0100

    genesysmod to open entrance scripts

---
+++ GENEeSYS-MOD/scripts/genesysmod_to_iamc/data_wrapper.py
@@ -0,0 +1,79 @@
+import pandas as pd
+import genesysmod_to_iamc.pyam_aggregator as pa
+
+from genesysmod_to_iamc._statics import *
+
+
+class DataWrapper(object):
+
+    def __init__(self, input_file, output_gdx):
+        self._output_gdx = output_gdx
+
+        self.usage_values = pd.DataFrame()
+        self.production_values = pd.DataFrame()
+        self.emission_values = pd.DataFrame()
+        self.capacity_values = pd.DataFrame()
+        self.cost_values = pd.DataFrame()
+
+        self.transformed_data = {}
+        self.map_timeslices = {}
+        self.demand_series = {}
+        self.output_series = {}
+
+        self.reinit_output_columns()
+        self.init_time_slices()
+
+        self.input_file = input_file
+        self.hourly_data_excel = None
+
+        self.idataframe = None
+
+    def reinit_output_columns(self):
+        _raw_production_values: pd.DataFrame = self._output_gdx[DEF_PRODUCTION_SHEET]
+        _raw_production_values.columns = DEF_PRODUCTION_COLUMNS
+
+        # Filter to corresponding type
+        _usage_values: pd.DataFrame = _raw_production_values[_raw_production_values['type'] == 'Use']
+        _production_values: pd.DataFrame = _raw_production_values[_raw_production_values['type'] == 'Production']
+
+        # Filter output units
+        self.usage_values: pd.DataFrame = _usage_values[_usage_values['unit'] == 'PJ']
+        self.production_values: pd.DataFrame = _production_values[_production_values['unit'] == 'PJ']
+
+        _raw_emission_values: pd.DataFrame = self._output_gdx[DEF_EMISSION_SHEET]
+        _raw_emission_values.columns = DEF_EMISSION_COLUMNS
+        self.emission_values = _raw_emission_values
+
+        _raw_capacity_values: pd.DataFrame = self._output_gdx[DEF_CAPACITY_SHEET]
+        _raw_capacity_values.columns = DEF_CAPACITY_COLUMNS
+        self.capacity_values = _raw_capacity_values
+
+        _raw_capacity_values: pd.DataFrame = self._output_gdx[DEF_CAPACITY_SHEET]
+        _raw_capacity_values.columns = DEF_CAPACITY_COLUMNS
+        self.capacity_values = _raw_capacity_values
+
+        _raw_cost_values: pd.DataFrame = self._output_gdx[DEF_EXOGENOUS_COST_SHEET]
+        _raw_cost_values.columns = DEF_EXOGENOUS_COST_COLUMNS
+        self.cost_values = _raw_cost_values
+
+    def init_time_slices(self):
+        timeslices = self.production_values['subannual'].unique()
+        map_timeslices = {}
+        counter = 0
+        for entry in timeslices:
+            counter += 1
+            map_timeslices[entry] = 'Reduced|t' + str(counter)
+
+        self.map_timeslices = map_timeslices
+
+    def write_all_transformed_csv(self):
+        for file in self.transformed_data:
+            filename = file + ".csv"
+            self.transformed_data[file].to_csv(DEF_OUTPUT_PATH / filename, sep=';')
+
+    def generate_idataframe(self, filter_only_yearly_values=False):
+        self.idataframe = pa.generate_idataframe(self, filter_only_yearly_values)
+
+    def generate_idataframe_renewable_series(self):
+        self.idataframe = pa.generate_idataframe_renewable_series(self)
+
---
+++ GENEeSYS-MOD/scripts/genesysmod_to_iamc/series_generator.py
@@ -0,0 +1,89 @@
+import genesysmod_to_iamc.data_reader as dr
+import genesysmod_to_iamc.data_wrapper as dw
+import pandas as pd
+import pyam
+
+from genesysmod_to_iamc._statics import *
+
+
+def generate_final_energy_series(data_wrapper: dw.DataWrapper, series_name: str, fuel_name:str):
+    _finalenergy_demand = data_wrapper.idataframe
+
+    _filtered = _finalenergy_demand[_finalenergy_demand['subannual'] == 'Year']
+    _filtered = _filtered[_filtered['variable'] == fuel_name]
+    _filtered = _filtered.groupby(['region', 'year']).sum().reset_index()
+    _filtered.head(10)
+
+    for year in DEF_YEARS:
+        _series = data_wrapper.demand_series[series_name].copy()
+        _series['unit'] = 'EJ/yr'
+        _series['model'] = DEF_MODEL_AND_VERSION
+        _series['fuel'] = fuel_name
+        _series['scenario'] = DEF_MAP_FILE_SCENARIOS[data_wrapper.input_file]
+        _series['year'] = year
+        # group by relevant values and take the sum
+
+        _frame = _series.groupby(
+            ['model', 'scenario', 'region', 'fuel', 'unit', 'subannual', 'year']).sum().reset_index()
+        _frame.columns = ['Model', 'Scenario', 'Region', 'Variable', 'Unit', 'Subannual', 'Year', 'Value']
+
+    regions = _series['region'].unique()
+    outputs = []
+    iso2_mapping = dr.loadmap_iso2_countries()
+
+    for year in DEF_YEARS:
+        for region in regions:
+            _yearly = _filtered.copy()
+            if region == 'UK':
+                region = 'GB'
+
+            if region == 'NONEU_Balkan':
+                region_key = 'Non-EU-Balkans'
+            else:
+                region_key = iso2_mapping[region]
+
+            _yearly = _yearly[(_yearly['region'] == region_key) & (_yearly['year'] == year)]
+            if not _yearly.empty:
+                _multiplier = _yearly.iloc[0].value
+
+                _filtered_series = _frame[(_frame['Region'] == region)].copy()
+                _filtered_series['Year'] = year
+                _filtered_series['Value'] = _filtered_series['Value'] * _multiplier
+                _filtered_series['Unit'] = 'EJ/yr'
+
+                outputs.append(_filtered_series.copy())
+
+    output_frame = pd.concat(outputs)
+
+    output_frame = output_frame.replace({'Region': 'UK'}, 'GB')
+    output_frame = output_frame.replace({'Region': 'NONEU_Balkan'}, 'Non-EU-Balkans')
+
+    for r in iso2_mapping:
+        output_frame = output_frame.replace({'Region': r}, iso2_mapping[r])
+
+    data_wrapper.output_series[fuel_name] = output_frame
+
+    output_frame.to_csv(series_name + ".csv")
+    return output_frame
+
+
+def generate_electricity_series(data_wrapper: dw.DataWrapper):
+    values = pd.concat(data_wrapper.output_series)
+    values = values.groupby(
+        ['Model', 'Scenario', 'Region', 'Variable', 'Unit', 'Subannual', 'Year']).sum().reset_index()
+    values = values.groupby(['Model', 'Scenario', 'Region', 'Unit', 'Subannual', 'Year']).sum().reset_index()
+    values['Variable'] = "Final Energy|Electricity"
+    values = values.groupby(
+        ['Model', 'Scenario', 'Region', 'Variable', 'Unit', 'Subannual', 'Year']).sum().reset_index()
+    values.columns = ['Model', 'Scenario', 'Region', 'Variable', 'Unit', 'Subannual', 'Year', 'Value']
+
+    data_wrapper.output_series['Final Energy|ElectricityDummy'] = values
+
+
+def output_series(data_wrapper: dw.DataWrapper):
+    values = pd.concat(data_wrapper.output_series).reset_index().drop(['level_0','level_1'], axis=1)
+    idataframe = pyam.IamDataFrame(values)
+
+    output_file = data_wrapper.input_file + "_series.csv"
+
+    idataframe.to_csv(DEF_OUTPUT_PATH / output_file)
